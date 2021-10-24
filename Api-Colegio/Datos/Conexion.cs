3f4894ecb3b8c869ee3e23d68e6aab7bc4d788a4
Antony Morales <emoralesf4@miumg.edu.gt>
2021-10-24 09:59:25 -0600

    Cadena de Conexion

---
+++ Api-Colegio/Datos/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        private static string Usuario = "sa";
+        private static string Password = "ANTONY27";
+        private static string Servidor = "DESKTOP-TQPJ3ML\\SQLEXPRESS";
+        private static string BD = "BDColegio";
+        public static string ObtenerCadenaConexionSQL()
+        {
+            return "Persist Security Info = False; User ID = '" + Usuario
+                    + "'; Password = '" + Password
+                    + "'; Initial Catalog = '" + BD
+                    + "'; Server = '" + Servidor + "'";
+        }
+
+    }
+}
